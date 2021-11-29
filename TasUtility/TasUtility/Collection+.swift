1757428450fa93163c1e6b5883915df2b41b16df
SkyTitan <er65119@naver.com>
2021-11-29 19:50:43 +0900

    Add Collection extension

---
+++ TasUtility/TasUtility/Collection+.swift
@@ -0,0 +1,14 @@
+//
+//  Collection+.swift
+//  TasUtility
+//
+//  Created by 박준현 on 2021/11/29.
+//
+
+import UIKit
+
+extension Collection {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
